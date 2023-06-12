d5929727e730ff092e51ed6be811618ecd4c65e2
kazuto1017 <kazuto.noguti@sapex.co.jp>
2023-06-12 17:27:57 +0900

    講座購入処理

---
+++ team-EC/src/main/java/test/ex/models/dao/TransactionItemDao.java
@@ -0,0 +1,15 @@
+package test.ex.models.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jakarta.transaction.Transactional;
+import test.ex.models.entity.TransactionItemEntity;
+
+
+public interface TransactionItemDao extends JpaRepository<TransactionItemEntity, Long> {
+	TransactionItemEntity save(TransactionItemEntity transactionItemEntity);
+	@Transactional
+	List<TransactionItemEntity> deleteByTransactionId(Long transactionId);
+}
