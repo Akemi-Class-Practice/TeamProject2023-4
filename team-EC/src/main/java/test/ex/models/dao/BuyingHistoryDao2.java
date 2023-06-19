d8ed32bda7d72bc7c7f289018480952983f83bb1
kuriwaki0419 <ryuto614182@gmail.com>
2023-06-19 13:50:44 +0900

    admin関連ファイルの名前とコード修正

--- team-EC/src/main/java/test/ex/models/dao/BuyingHistoryDao2.java
+++ team-EC/src/main/java/test/ex/models/dao/BuyingHistoryDao2.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< HEAD
+
+
 import test.ex.models.entity.BuyingHistoryEntity;
 import test.ex.models.entity.BuyingHistoryKey;
 
-public interface BuyingHistoryDao2 extends JpaRepository<BuyingHistoryEntity, BuyingHistoryKey> {
-=======
+
 
 import test.ex.models.entity.BuyingCartCheckEntity;
 import test.ex.models.entity.BuyingCartKey;
@@ -17,16 +17,18 @@ import test.ex.models.entity.BuyingHistoryEntity;
 import test.ex.models.entity.BuyingHistoryKey;
 
 public interface BuyingHistoryDao2 extends JpaRepository<BuyingCartCheckEntity,BuyingCartKey> {
->>>>>>> d0ac03d18c02951988f34cbdb474ff0e360ce185
+
+
 
     @Query(value = "SELECT th.student_id, ti.lesson_id " +
                    "FROM transaction_history AS th " +
                    "JOIN transaction_item AS ti ON th.transaction_id = ti.transaction_id " +
                    "WHERE th.student_id = ?1",
                    nativeQuery = true)
-<<<<<<< HEAD
-    List<Object[]> findStudentIdAndLessonIdByStudentId(Long studentId);
-=======
+
+
+
     List<BuyingCartCheckEntity> findStudentIdAndLessonIdByStudentId(Long studentId);
->>>>>>> d0ac03d18c02951988f34cbdb474ff0e360ce185
+
+
 }
