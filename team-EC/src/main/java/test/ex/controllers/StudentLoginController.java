76b3ed0c58b7a655c7ccf7ac626556a369b80368
kazuto1017 <kazuto.noguti@sapex.co.jp>
2023-06-19 18:02:08 +0900

    ユーザー側更新

---
+++ team-EC/src/main/java/test/ex/controllers/StudentLoginController.java
@@ -0,0 +1,51 @@
+package test.ex.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import test.ex.models.entity.StudentEntity;
+import test.ex.service.StudentService;
+
+@Controller
+public class StudentLoginController {
+
+	@Autowired
+	private StudentService studentService;
+
+	@Autowired
+	HttpSession session;
+
+	// ログイン画面の表示-----------------------------------------------------
+	@GetMapping("/student/login")
+	public String getLoginPage() {
+		return "userLogin.html";
+	}
+
+	// ログイン処理-----------------------------------------------------------------------------------
+
+	@PostMapping("/student/login/process")
+	public String login(@RequestParam String email, @RequestParam String password, Model model) {
+		// studentServiceクラスのfindByEmailAndPasswordメソッドを使用して、該当するユーザー情報を取得する。
+		StudentEntity studentEntity = studentService.selectByEmailAndPassword(email, password);
+		if (studentEntity == null) {
+			// 入力されたメールアドレスまたはパスワードが存在しなかった場合
+			//// errorをtrueにする（HTML側でエラーメッセージを出力するため）
+			model.addAttribute("error", true);
+			return "userLogin.html";
+		} else {
+			// 入力されたメールアドレスまたはパスワードが存在した場合
+			// studentEntityの内容をsessionに保存する
+			session.setAttribute("student", studentEntity);
+			// errorをfalseにする（HTML側でエラーメッセージを出力させないため）
+			model.addAttribute("error", false);
+			return "redirect:/student/lesson/list";//暫定的にレジスターに飛ばす後で変更
+		}
+
+	}
+
+}
---
+++ team-EC/src/main/java/test/ex/controllers/StudentPointController.java
@@ -0,0 +1,76 @@
+package test.ex.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import test.ex.models.dao.StudentDao;
+import test.ex.models.entity.StudentEntity;
+
+
+@Controller
+public class StudentPointController {
+    private final StudentDao studentDao;
+    
+    @Autowired
+	HttpSession session;
+    
+
+    @Autowired
+    public StudentPointController(StudentDao studentDao){ 
+        this.studentDao = studentDao;
+    }
+
+	//階層へ遷移用 今現在のポイント取得
+    @GetMapping("/pointsuccess")
+    public String fix(Model model) {
+        StudentEntity userList = (StudentEntity) session.getAttribute("student");
+        Long student_id = userList.getStudentId();
+        StudentEntity student = studentDao.findByStudentId(student_id);
+        model.addAttribute("point", student.getPoint());
+        return "point-fix.html";
+    }
+
+	//階層へ遷移用 今現在のポイント取得
+    @GetMapping("/point")
+    public String point(Model model) {
+        StudentEntity userList = (StudentEntity) session.getAttribute("student");
+        Long student_id = userList.getStudentId();
+        StudentEntity student = studentDao.findByStudentId(student_id);
+        model.addAttribute("point", student.getPoint());
+        return "userPoint.html";
+    }
+    
+    
+    
+    
+    //ポイント追加処理
+    @PostMapping("/point")
+    public String addPoint(@RequestParam("pointAmount") int pointAmount,Model model) {
+        
+        //ログインしているユーザーを取得
+        StudentEntity userList = (StudentEntity) session.getAttribute("student");
+		Long student_id = userList.getStudentId();
+
+        // ユーザの情報をデータベースから取得  後ほど修正かも
+        StudentEntity student = studentDao.findByStudentId(student_id);
+
+
+        // 現在のポイントを取得
+        int currentPoint = student.getPoint();
+
+        // ポイントを増やす
+        int newPoint = currentPoint + pointAmount;
+        student.setPoint(newPoint);
+     
+
+        // データベースに変更を保存
+        studentDao.save(student);
+
+        // 成功した場合
+        return "redirect:/pointsuccess";
+    }
+}
