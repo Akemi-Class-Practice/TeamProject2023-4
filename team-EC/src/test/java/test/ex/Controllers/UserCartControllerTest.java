2f7e6c225a61095331ab8be7c04f3f6c65fa85f2
kazuto1017 <kazuto.noguti@sapex.co.jp>
2023-06-22 09:59:50 +0900

    削除

--- team-EC/src/test/java/test/ex/Controllers/UserCartControllerTest.java
+++ team-EC/src/test/java/test/ex/Controllers/UserCartControllerTest.java
@@ -1,14 +1,21 @@
-package controller;
+package test.ex.Controllers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import jakarta.servlet.http.HttpSession;
 import test.ex.models.entity.StudentEntity;
 import test.ex.service.LessonService;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserCartControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
@@ -23,6 +30,15 @@ public class UserCartControllerTest {
 	public void perpareData() {
 		
 		
+	}
+	
+	
+	
+	//MyPageに遷移しない(Loginしていない)
+	@Test
+	public void testGetUserRegisterPage_UnSuccessful() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/user/cart/list");
+		mockMvc.perform(request).andExpect(redirectedUrl("/user/login"));
 	}
 	
 	//ユーザーがログインしている状態でカート一覧ページに遷移できるかどうか
